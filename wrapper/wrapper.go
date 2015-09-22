3c78f900219d93ee8b85db9cef97c567e1c7ddf8
David K. Bainbridge <dbainbri@ciena.com>
2015-09-22 16:56:53 -0700

    cleanup unused code and log messages

--- wrapper/wrapper.go
+++ wrapper/wrapper.go
@@ -27,12 +27,19 @@ import (
 )
 
 const (
-	clusterFileName = "/bp2/hooks/cluster.json"
-	tabletsFileName = "/bp2/hooks/tablets.json"
+	onosHome            = "/root/onos"
+	cleanOnosArchive    = "/bp2/save/clean.tgz"
+	onosConfigDir       = "/root/onos/config"
+	clusterFileName     = "/bp2/hooks/cluster.json"
+	onosClusterConfig   = "/root/onos/config/cluster.json"
+	partitionFileName   = "/bp2/hooks/tablets.json"
+	onosPartitionConfig = "/root/onos/config/tablets.json"
+	serveOnAddr         = "127.0.0.1:4343"
 )
 
-func echo(from io.ReadCloser, to *os.File) {
-	log.Printf("ABOUT TO READ %v\n", from)
+// echo take what is ever on one reader and write it to the writer
+func echo(from io.ReadCloser, to io.Writer) {
+	log.Printf("INFO: about to start reading from %v\n", from)
 	buf := make([]byte, 1024)
 	for {
 		if cnt, err := from.Read(buf); cnt >= 0 && err == nil {
@@ -42,9 +49,10 @@ func echo(from io.ReadCloser, to *os.File) {
 		}
 	}
 	from.Close()
-	log.Printf("DONE READING\n")
+	log.Printf("INFO: done reading from %v\n", from)
 }
 
+// endsId true if the string ends in one of the suffixes in the list, else false
 func endsIn(v string, l []string) bool {
 	for _, t := range l {
 		if strings.HasSuffix(v, t) {
@@ -54,6 +62,7 @@ func endsIn(v string, l []string) bool {
 	return false
 }
 
+// in true if the string is in the list, else false
 func in(v string, l []string) bool {
 	for _, t := range l {
 		if t == v {
@@ -63,110 +72,96 @@ func in(v string, l []string) bool {
 	return false
 }
 
+// wipe removes all files form the specified directory, but not the directory, or if the specified path is a normal
+// file, that file is just deleted.
 func wipe(path string, suffixlist []string, excludes []string) {
-	log.Printf("ATTEMPTING TO CLEAN: %s\n", path)
+	log.Printf("INFO: wiping '%s'\n", path)
 	if f, err := os.Stat(path); err == nil {
 		if f.IsDir() {
 			if infos, err := ioutil.ReadDir(path); err != nil {
-				//log.Fatalf("Unable to clean up %s: %s\n", path, err)
-				log.Printf("WARN: Unable to clean up %s: %s\n", path, err)
+				log.Printf("WARN: unable to wipe '%s': %s\n", path, err)
 			} else {
 				for _, info := range infos {
 					if !endsIn(info.Name(), suffixlist) && !in(info.Name(), excludes) {
-						log.Printf("REMOVE: %s\n", path+"/"+info.Name())
+						log.Printf("INFO: removing: '%s'\n", path+"/"+info.Name())
 						if err := os.RemoveAll(path + "/" + info.Name()); err != nil {
-							log.Printf("ERROR: unable to remove %s\n", path+"/"+info.Name())
+							log.Printf("ERROR: unable to remove '%s': %s\n", path+"/"+info.Name(), err)
 						}
 					}
 				}
 			}
 		} else {
 			if err := os.Remove(path); err != nil {
-				log.Printf("ERROR: unable to remove %s\n", path)
+				log.Printf("ERROR: unable to remove '%s': %s\n", path, err)
 			}
 		}
 	} else {
-		log.Printf("ERROR: unable to stat file: %s\n", path)
+		log.Printf("ERROR: unable to stat file: '%s': %s\n", path, err)
 	}
 }
+
+// startOnos provides a uniform process for starting a clean ONOS instance. this assumes the existing instance has
+// been killed. the following steps are taken:
+//     1. clean up the /tmp directory
+//     2. remove the ONOS installation
+//     3. install a fresh / clean ONOS installation from a a save tree
+//     4. install the updated cluster and partition configuration files
+//     5. start the ONOS process
 func startOnos() *exec.Cmd {
 
+	log.Println("INFO: wiping /tmp directory")
 	wipe("/tmp", []string{}, []string{})
 
-	// wipe("/root/onos/apache-karaf-3.0.3/data", []string{}, []string{})
-	// wipe("/root/onos/apache-karaf-3.0.3/instances", []string{}, []string{})
-	// wipe("/root/onos/apache-karaf-3.0.3/lock", []string{}, []string{})
-
-	// log.Println("SAVING CONFIG")
-	// save := exec.Command("tar", "-P", "-zcf", "/bp2/save/save.tgz", "/root/onos/config/cluster.json", "/root/onos/config/tablets.json")
-	// if err := save.Run(); err != nil {
-	// 	log.Printf("ERROR: unable to save config")
-	// }
-
-	log.Println("PURGE ONOS")
-	clean := exec.Command("rm", "-rf", "/root/onos")
-	if err := clean.Run(); err != nil {
-		log.Printf("ERROR: unable to remove tree")
+	log.Println("INFO: wiping ONOS installation from /root/onos")
+	if err := os.RemoveAll(onosHome); err != nil {
+		log.Printf("ERROR: unable to remove ONOS installation: %s\n", err)
 	}
 
-	log.Println("RESTORE FROM CLEAN ONOS")
-	restore := exec.Command("tar", "-P", "-zxf", "/bp2/save/clean.tgz")
+	// this is a bit of a hack. there is a clean copy of the source tree in a TAR file, so just call the OS
+	// tar command to untar the archive to the correct location. might be cleaner (more portable) it we used
+	// the Go tar package, but for this we don't need to work that hard.
+	log.Println("INFO: restoring ONOS from a clean archive")
+	restore := exec.Command("tar", "-P", "-zxf", cleanOnosArchive)
 	if err := restore.Run(); err != nil {
-		log.Printf("ERROR: unable to restore from clean")
+		log.Printf("ERROR: unable to restore from clean archive: %s\n", err)
 	}
 
-	// log.Println("RESTORE FROM CONFIG")
-	// restorec := exec.Command("tar", "-P", "-zxf", "/bp2/save/save.tgz")
-	// if err := restorec.Run(); err != nil {
-	// 	log.Printf("ERROR: unable to restore config")
-	// }
+	log.Println("INFO: create ONOS cluster configuration directory")
+	if err := os.MkdirAll(onosConfigDir, 0755); err != nil {
+		log.Printf("ERROR: unable to create ONOS cluster configuration directory: %s\n", err)
+	}
 
-	log.Println("copy service")
-	cp := exec.Command("cp", "/bp2/hooks/onos-service", "/root/onos/bin")
+	// this is a bit of a hack. we could implement our own file copy func in Go, but instead we will use a
+	// system call. this is not portable, but for now should be good enough
+	log.Println("INFO: copy desired cluster configuration to ONOS configuration area")
+	cp := exec.Command("cp", clusterFileName, onosClusterConfig)
 	if err := cp.Run(); err != nil {
-		log.Printf("ERROR: unable to copy my service %s\n", err)
+		log.Printf("ERROR: unable to copy desired cluster config into ONOS: %s\n", err)
 	}
 
-	log.Println("make dir")
-	md := exec.Command("mkdir", "-p", "/root/onos/config")
-	if err := md.Run(); err != nil {
-		log.Printf("ERROR: unable to mkdir %s\n", err)
-	}
-
-	log.Println("copy cluster.json")
-	cp = exec.Command("cp", clusterFileName, "/root/onos/config/cluster.json")
+	// this is a bit of a hack. we could implement our own file copy func in Go, but instead we will use a
+	// system call. this is not portable, but for now should be good enough
+	log.Println("INFO: copy desired cluster partition configuration to ONOS configuration area")
+	cp = exec.Command("cp", partitionFileName, onosPartitionConfig)
 	if err := cp.Run(); err != nil {
-		log.Printf("ERROR: unable to copy my cluster config %s\n", err)
+		log.Printf("ERROR: unable to copy desired cluster partition config into ONOS: %s\n", err)
 	}
 
-	// rm := exec.Command("rm", clusterFileName)
-	// if err := rm.Run(); err != nil {
-	// 	log.Printf("ERROR: unable to remove cluster config %s\n", err)
-	// }
-
-	log.Println("copy tablets.json")
-	cp = exec.Command("cp", tabletsFileName, "/root/onos/config/tablets.json")
-	if err := cp.Run(); err != nil {
-		log.Printf("ERROR: unable to copy my tablets: %s\n", err)
-	}
-
-	// rm = exec.Command("rm", tabletsFileName)
-	// if err := rm.Run(); err != nil {
-	// 	log.Printf("ERROR: unable to remove tablets config %s\n", err)
-	// }
-
+	// Execute ONOS as a separate process. we allow for optional arguments to be specified to to ONOS
 	onos := exec.Command(os.Args[1], os.Args[2:]...)
 	onos.Env = nil
-	onos.Dir = "/root/onos"
+	onos.Dir = onosHome
+
+	// Create readers for stdin and stderr
 	if pipe, err := onos.StdoutPipe(); err == nil {
 		go echo(pipe, os.Stdout)
 	} else {
-		log.Printf("ERROR: unable to read stdout %s\n", err)
+		log.Printf("ERROR: unable to read ONOS stdout: %s\n", err)
 	}
 	if pipe, err := onos.StderrPipe(); err == nil {
 		go echo(pipe, os.Stderr)
 	} else {
-		log.Printf("ERROR: unable to read stderr %s\n", err)
+		log.Printf("ERROR: unable to read ONOS stderr: %s\n", err)
 	}
 
 	if err := onos.Start(); err != nil {
@@ -177,32 +172,38 @@ func startOnos() *exec.Cmd {
 
 func main() {
 
-	log.Println("WRAPPER COMMIT: 0009")
-	log.Printf("COMMAND: %s", os.Args[1])
-	log.Printf("ARGS: %v", os.Args[2:])
-
+	log.Printf("INFO: starting ONOS with command '%s' and arguments '%v'\n", os.Args[1], os.Args[2:])
 	onos := startOnos()
 
+	// Start a routine that will monitor the ONOS process and when it stops will restart it
 	go func() {
 		for {
-			log.Println("Waiting for processes to complete")
+			log.Printf("INFO: waiting for processes to complete: '%d'\n", onos.Process.Pid)
 			onos.Wait()
-			log.Println("Process complete, restarting")
+			log.Printf("INFO: ONOS process '%d' complete with '%s', restarting\n",
+				onos.Process.Pid, onos.ProcessState.String())
 			onos = startOnos()
 		}
 	}()
 
+	// register a handler for the /reset REST call. this will kill the ONOS process which will automatically restart
 	http.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Attempting to kill %v\n", onos.Process)
-		onos.Process.Kill()
+		log.Printf("INFO: killing ONOS process '%d'\n", onos.Process.Pid)
+		if err := onos.Process.Kill(); err != nil {
+			log.Printf("ERROR: failed to kill ONOS process, bad things about to happen: %s\n", err)
+		}
 	})
+
+	// register a handler for the /stop REST call, which will stop the wrapper process
 	http.HandleFunc("/stop", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Attempting to kill %v\n", onos.Process)
-		onos.Process.Kill()
-		log.Printf("Stopping wrapper")
+		log.Printf("INFO: killing ONOS process '%d'\n", onos.Process.Pid)
+		if err := onos.Process.Kill(); err != nil {
+			log.Printf("ERROR: failed to kill ONOS process, parent about to exit: %s\n", err)
+		}
+		log.Println("INFO: halting ONOS wrapper")
 		os.Exit(0)
 	})
 
-	log.Printf("About to listen and serve")
-	log.Fatalf("Failed to start API server: %s\n", http.ListenAndServe("127.0.0.1:4343", nil))
+	log.Printf("INFO: listen and serve REST requests on '%s'\n", serveOnAddr)
+	log.Fatalf("ERROR: failed to start API server on '%s': %s\n", serveOnAddr, http.ListenAndServe(serveOnAddr, nil))
 }
